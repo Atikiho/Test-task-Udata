f98dc89c9ead006ec9ac3ff4ae8be860f742b3c1
user <nikitaskrypets@gmail.com>
2025-02-18 14:13:13 +0200

    created utils

---
+++ API/utils.py
@@ -0,0 +1,13 @@
+import json
+from typing import List
+
+from fastapi import HTTPException
+
+
+def read_products_file() -> List[dict]:
+    try:
+        with open("products.json", "r", encoding="utf-8") as file:
+            data = json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        raise HTTPException(status_code=500, detail=f"Data file is missing or invalid")
+    return data
